48cd2a337f525767988ed1aa774925c6b0ddc6e2
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-12-06 13:23:09 -0800

    posts db & files in places

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -17,3 +18,13 @@ CREATE TABLE github_users (
   email TEXT,
   avatar TEXT
 );
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  description VARCHAR(255) NOT NULL
+)
+
+insert into posts (
+  description
+) VALUES
+('Im just a post. Yes Im only a post. And Im sitting here on backend-gitty. Well, its a long, long journety to the heroku deploy...')
